f230a1a3d1b1519e8b37c1a1be79c2aae6e6de53
lucas hernandez <ih7251010@gmail.com>
2019-04-29 17:54:22 -0300

    logica de programaçao

---
+++ lita.js
@@ -0,0 +1,31 @@
+// tipo array vazio
+
+let listaVazia =[];
+
+//tipo arrey valores definidos
+let listavazia =[1,2,3, 'lucas hernandez'];
+//buscar valor dentro do array
+retorna o valor
+let item =listaValor.find(f => f=='lucas hernandez')
+
+console.log(intem)
+let itemfilter =listavalor.filter(i=> i 'lucas');
+
+console.log(itemfilter);
+console.log(listavalo)
+// me informa o tamanho
+console.log(listavalor.length)
+
+//adicionar itens na fila 
+
+
+
+//excluir itens da lista <-ultimo
+listavalor.pop();
+console.log(listavalor);
+
+//excliuir determinada posiçao 
+listavalor.remove(lucas);
+
+console.log(index);
+listavalor =listavalor.splice(0,index);
---
+++ regras.js
@@ -0,0 +1,15 @@
+//string
+let nome = 'lucas hernandez',
+
+//int -> inteiro
+let idade = 15,
+
+//numero decimal =7.5,
+let decimal = 7.5
+
+// verdeiro/falso
+let verdairo = true,
+
+
+
+console.log(idade),
